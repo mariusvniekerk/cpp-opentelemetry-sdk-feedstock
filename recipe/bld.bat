5931d741e485f7412dba9c26ada65da41cdd779a
David Li <li.davidm96@gmail.com>
2021-12-13 13:11:25 -0500

    Get protos from /share not /Library/share

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,15 +3,8 @@
 mkdir build-cpp
 if errorlevel 1 exit 1
 
-echo %CONDA_PREFIX%
-echo %LIBRARY_PREFIX%
-dir %CONDA_PREFIX%\Library\share
-dir %LIBRARY_PREFIX%\share
-dir %CONDA_PREFIX%\share
-dir %PREFIX%\share
-
 REM Release tarballs do not contain the required Protobuf definitions.
-robocopy /S /E %CONDA_PREFIX%\Library\share\opentelemetry\opentelemetry-proto\opentelemetry .\third_party\opentelemetry-proto\opentelemetry
+robocopy /S /E %PREFIX%\share\opentelemetry\opentelemetry-proto\opentelemetry .\third_party\opentelemetry-proto\opentelemetry
 REM Stop CMake from trying to git clone the Protobuf definitions.
 mkdir .\third_party\opentelemetry-proto\.git
 
