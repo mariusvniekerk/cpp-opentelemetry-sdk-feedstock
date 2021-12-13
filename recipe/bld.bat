3013994ffcd2007c982b6d53219a509b905944e0
David Li <li.davidm96@gmail.com>
2021-12-13 12:30:04 -0500

    TEMP: try to work out difference between CI and local paths

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,6 +3,13 @@
 mkdir build-cpp
 if errorlevel 1 exit 1
 
+echo %CONDA_PREFIX%
+echo %LIBRARY_PREFIX%
+dir %CONDA_PREFIX%\Library\share
+dir %LIBRARY_PREFIX%\share
+dir %CONDA_PREFIX%\share
+dir %PREFIX%\share
+
 REM Release tarballs do not contain the required Protobuf definitions.
 robocopy /S /E %CONDA_PREFIX%\Library\share\opentelemetry\opentelemetry-proto\opentelemetry .\third_party\opentelemetry-proto\opentelemetry
 REM Stop CMake from trying to git clone the Protobuf definitions.
