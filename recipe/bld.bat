cb9fa3bcc7fda7a351ca12a84f94400ffcae42fc
David Li <li.davidm96@gmail.com>
2021-12-13 13:28:49 -0500

    Use the right prefix

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,7 +4,7 @@ mkdir build-cpp
 if errorlevel 1 exit 1
 
 REM Release tarballs do not contain the required Protobuf definitions.
-robocopy /S /E %PREFIX%\share\opentelemetry\opentelemetry-proto\opentelemetry .\third_party\opentelemetry-proto\opentelemetry
+robocopy /S /E %BUILD_PREFIX%\share\opentelemetry\opentelemetry-proto\opentelemetry .\third_party\opentelemetry-proto\opentelemetry
 REM Stop CMake from trying to git clone the Protobuf definitions.
 mkdir .\third_party\opentelemetry-proto\.git
 
